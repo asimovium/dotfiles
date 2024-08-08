db6c5ffa380bc37ce99f8c3fc1f23abdede182d4
JVL de Oliveira <joaovitordelima81@usp.br>
2024-08-08 17:17:12 -0300

    ai

---
+++ nvim/lua/user/plugins-settings/packer.lua
@@ -0,0 +1,101 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use "wbthomason/packer.nvim"  -- Have packer manage itself
+  use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
+
+  -- colorscheme
+  use "folke/tokyonight.nvim"
+  use "Mofiqul/dracula.nvim"    -- Dracula colorscheme
+  use "GlennLeo/cobalt2"      -- Cobalt2 colorscheme
+
+    -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- treesitter
+  use "nvim-treesitter/nvim-treesitter"
+
+  -- Auto pairs
+  use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+    }
+
+  -- Rainbow parenthesis
+  use "HiPhish/rainbow-delimiters.nvim" -- Essa merda n funciona
+
+  -- Nvim-tree
+  use "nvim-tree/nvim-tree.lua"
+  use "nvim-tree/nvim-web-devicons"
+  
+  -- Buffers
+    use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+    
+  -- Powerline
+    use {'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true }}
+
+  -- Terminal
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+  require("toggleterm").setup()
+    end}
+
+ -- Indent line
+ use "lukas-reineke/indent-blankline.nvim"
+
+ -- Comment
+ use "numToStr/Comment.nvim"  
+ use "JoosepAlviste/nvim-ts-context-commentstring"
+
+ -- Run code
+ use 'CRAG666/code_runner.nvim'
+
+
+
+
+
+   -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
